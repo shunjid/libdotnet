cc69230f44555535d8807afac951e668ea43e0cc
shunjid <shunjid.rahman@gmail.com>
2021-04-20 19:35:15 +0600

    seeded mock data from database

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace libdotnet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        
+    }
+}
